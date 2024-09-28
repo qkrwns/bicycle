622083708831c6ea3c7382106dafa242a9b3623b
l1l1l1lI <156498931+chominhu@users.noreply.github.com>
2024-09-28 13:34:18 +0900

    asdf

--- bicycle-front/src/views/Chatting.vue
+++ bicycle-front/src/views/Chatting.vue
@@ -5,14 +5,37 @@ export default {
     }),
   }
 </script>
+<style>
+  .chatting-box {
+    width: 30%;
+    min-height: 180px;
+    margin: 20px 20px 0;
+    border: 1px solid #333;
+    border-radius: 25px;
+    padding: 20px;
+  }
+  
+  .chatting-box.me {
+    margin-left: auto;
+  }
+</style>
 
-
-    <template>
-        <v-container >
-            <h2>채팅창</h2>
-            <div class="bg-deep-purple-lighten-5 border-sm" style="height: 500px; width: 100%; border-color: red;"></div>
-            <v-textarea class="mt-1">asdf</v-textarea>
-            <v-btn>전송</v-btn>
-               
-        </v-container>
-    </template>
+<template>
+    <v-container >
+        <div class="d-flex align-center pa-5 text-h4 font-weight-black bg-white" style="min-height:100px; width:100%;">
+            <v-avatar image="https://w7.pngwing.com/pngs/710/71/png-transparent-profle-person-profile-user-circle-icons-icon-thumbnail.png" size="60"></v-avatar>&nbsp&nbsp**님과의 채팅
+          </div>
+        <div class="bg-deep-purple-lighten-5 border-sm main" style="height: 700px; width: 100%; border-color: red; overflow-y: scroll">
+          
+          <div class="bg-yellow chatting-box me">1</div>
+          <div class="bg-yellow chatting-box you">1</div>
+          <div class="bg-yellow chatting-box me">1</div>
+          <div class="bg-yellow chatting-box me">1</div>
+          <div class="bg-yellow chatting-box you">1</div>
+          <div class="bg-yellow chatting-box me">1</div>
+        </div>
+        <v-textarea class="mt-1" label="메세지를 입력하시오."></v-textarea>
+        <v-btn>전송</v-btn>
+           
+    </v-container>
+</template>
