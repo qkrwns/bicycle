ceb297eb29dec81de4083fe2a64b10a2f9c15f97
l1l1l1lI <156498931+chominhu@users.noreply.github.com>
2024-10-12 15:58:21 +0900

    adf

--- bicycle-front/src/views/Register.vue
+++ bicycle-front/src/views/Register.vue
@@ -1,9 +1,18 @@
 <script>
-export default {
-    data: () => ({
-      expand: false,
-    }),
+  export default {
+  name: 'App',
+  data(){
+    return{      
+      bicycle:'',
+      lost_date: '',
+      content: '',
+      location: '',
+    }
+  },
+  methods:{
+    
   }
+}
 </script>
 <style>
 </style>
@@ -11,13 +20,13 @@ export default {
 <template>
 <h1 class="pa-5">자전거 분실 등록 페이지</h1>
    <v-container fluid>
-    
+      <form method="POST" action="/">
       <v-col cols="6" >
         <v-list-subheader class="text-h6">자전거기종</v-list-subheader>
       </v-col>
                              
       <v-col cols="12">
-        <v-text-field ></v-text-field>
+        <v-text-field name="bicycle" v-model="bicycle"></v-text-field>
       </v-col>
     
 
@@ -25,44 +34,44 @@ export default {
 
     
       <v-col cols="6">
-        <v-list-subheader class="text-h6">잃어버린 위치</v-list-subheader>
+        <v-list-subheader  class="text-h6">잃어버린 위치</v-list-subheader>
       </v-col>
 
       <v-col cols="12">
         <v-text-field
-         
+          name="location" v-model="location"
         ></v-text-field>
       </v-col>
     
-
-    
       <v-col cols="6">
-        <v-list-subheader class="text-h6">잃어버린 시간</v-list-subheader>
+        <v-list-subheader  class="text-h6">잃어버린 시간</v-list-subheader>
       </v-col>
 
       <v-col cols="12">
         <v-text-field
           model-value="12:30:00"
           type="datetime-local"
-          
+          name="lost_date"
+          v-model="lost_date"
         ></v-text-field>
       </v-col>
 
       <v-col cols="4">
-        <v-list-subheader class="text-h6">설명</v-list-subheader>
+        <v-list-subheader class="text-h6" >설명</v-list-subheader>
       </v-col>
 
       <v-col cols="12">
         <v-textarea
-          
+          name="content"
+          v-model="content"
         ></v-textarea>
       </v-col>
 
       
-        <v-btn block
+        <v-btn type="submit" block
                 class="bg-success py-6 text-h5 font-weight-bold mt-4 elevation-6">
            등록
         </v-btn>
-      
+      </form>
   </v-container>
-</template>s
+</template>
--- bicycle-front/vite.config.js
+++ bicycle-front/vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  devServer: {
+    proxy: "http://127.0.0.1:5000"
+  }
 })
